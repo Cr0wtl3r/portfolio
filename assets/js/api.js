90b33b1741ddbb265601e267a107ac607f04b707
Albino Marques <albinomarques01@gmail.com>
2023-03-25 22:00:20 -0300

    Feitas atualização pertinentes a novas tecnologias, experiências e conhecimentos.

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,7 @@
 async function fetchProfileData() {
-  const url =
-    "https://raw.githubusercontent.com/Albino-Marques/portfolio/main/data/profile.json";
+  // const url =
+  //   "https://raw.githubusercontent.com/Albino-Marques/portfolio/main/data/profile.json";
+  const url = "data/profile.json"
   const fetching = await fetch(url);
   return await fetching.json();
 }
